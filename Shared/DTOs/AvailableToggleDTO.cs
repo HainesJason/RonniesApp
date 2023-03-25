4b2d904f2a3d3d853cae877dd2e4190c208d6868
Jason Haines <jason.haines@jcaglobal.com>
2023-03-25 21:32:34 +0000

    further date adjustments

--- Shared/DTOs/AvailableToggleDTO.cs
+++ Shared/DTOs/AvailableToggleDTO.cs
@@ -1,11 +1,13 @@
-﻿namespace SMS.Shared.DTOs
+﻿using System;
+
+namespace SMS.Shared.DTOs
 {
     public class AvailableToggleDTO
     {
         public int FixtureId { get; set; }
         public string Opponent { get; set; } = string.Empty;
         public string Venue { get; set; } = string.Empty;
-        public string DateOfFixture { get; set; }
+        public DateTime DateOfFixture { get; set; }
         public string StartTime { get; set; } = string.Empty;
         public bool CurrentAvailabilityStatus { get; set; }
     }
