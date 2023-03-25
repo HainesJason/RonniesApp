8782a1911da8ceab5c9baa77d2d0aa776cbc234d
Jason Haines <jason.haines@jcaglobal.com>
2023-03-25 21:16:55 +0000

    Updates

--- Shared/DTOs/Availability/FixtureCountSummaryDto.cs
+++ Shared/DTOs/Availability/FixtureCountSummaryDto.cs
@@ -1,10 +1,12 @@
-﻿namespace SMS.Shared.DTOs.Availability
+﻿using System;
+
+namespace SMS.Shared.DTOs.Availability
 {
     public class FixtureCountSummaryDto
     {
         public int FixtureId { get; set; }
         public string FixtureDetail { get; set; } = string.Empty;
-        public string DateOfFixture { get; set; }
+        public DateTime DateOfFixture { get; set; }
         public int PlayersAvailable { get; set; }
         public int PlayersRequired { get; set; }
     }
